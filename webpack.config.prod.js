f867bee20d04c847cacbe06ec52b56ea57480d91
Michael Lynch <mplynch@gmail.com>
2017-09-19 08:49:33 -0500

    Cleaned up production build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,6 @@
 var webpack = require('webpack');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   context: __dirname + '/app',
@@ -9,67 +10,76 @@ module.exports = {
       '@uirouter/angularjs',
       'angular',
       'angular-animate',
-      'angular-bootstrap',
+      'angular-ui-bootstrap',
+      'angular-ui-grid',
       'angular-loader',
+      'angular-filter',
       'bootstrap',
       'jquery']
-  },
-  output: {
-    path: __dirname + '/js',
-    filename: 'app.min.js'
-  },
-  module: {
-    loaders: [
-      // {
-      //   test: /\.css$/,
-      //   loader: 'style-loader!css-loader'
-      // },
-      {
+    },
+    output: {
+      path: __dirname + '/dist',
+      filename: 'app.min.js'
+    },
+    module: {
+      loaders: [
+        // {
+        //   test: /\.css$/,
+        //   loader: 'style-loader!css-loader'
+        // },
+        {
           test: /\.css$/,
           loader: ExtractTextPlugin.extract("css-loader", "postcss-loader")
-      },
-      {
-        test: /\.woff$/,
-        loader: 'url-loader?limit=100000'
-      },
-      {
-        test: /\.woff2$/,
-        loader: 'url-loader?limit=100000'
-      },
-      {
-        test: /\.ttf$/,
-        loader: 'url-loader?limit=100000'
-      },
-      {
-        test: /\.eot$/,
-        loader: 'file-loader'
-      },
-      {
-        test: /\.svg$/,
-        loader: 'url-loader?limit=100000'
-      },
-      {
-        test: /bootstrap.+\.(jsx|js)$/,
-        loader: 'imports-loader?jQuery=jquery,$=jquery,this=>window'
-      },
-      {
-        test: /\.html$/,
-        loader: 'ng-cache-loader?prefix=[dir]/[dir]'
-      }
+        },
+        {
+          test: /\.woff$/,
+          loader: 'url-loader?limit=100000'
+        },
+        {
+          test: /\.woff2$/,
+          loader: 'url-loader?limit=100000'
+        },
+        {
+          test: /\.ttf$/,
+          loader: 'url-loader?limit=100000'
+        },
+        {
+          test: /\.eot$/,
+          loader: 'file-loader'
+        },
+        {
+          test: /\.svg$/,
+          loader: 'url-loader?limit=100000'
+        },
+        {
+          test: /bootstrap.+\.(jsx|js)$/,
+          loader: 'imports-loader?jQuery=jquery,$=jquery,this=>window'
+        },
+        {
+          test: /\.html$/,
+          loader: 'ng-cache-loader?prefix=[dir]/[dir]'
+        }
+      ]
+    },
+    plugins: [
+      new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.bundle.js' }),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        }
+      }),
+      new webpack.ProvidePlugin({
+        jQuery: 'jquery',
+        $: 'jquery',
+        jquery: 'jquery'
+      }),
+      new ExtractTextPlugin("app.css"),
+      new CopyWebpackPlugin([
+        // {output}/file.txt
+        { from: '../index.html' },
+
+        // Copy directory contents to {output}/
+        { from: '../img', to: 'img' }
+      ])
     ]
-  },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.bundle.js' }),
-    new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            }
-        }),
-    new webpack.ProvidePlugin({
-      jQuery: 'jquery',
-      $: 'jquery',
-      jquery: 'jquery'
-    }),
-    new ExtractTextPlugin("app.css")
-  ]
-};
+  };
